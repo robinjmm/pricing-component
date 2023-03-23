8bef19bb8ef110ed018dd1bc2f15e071c7e70bcf
JMMR <103889221+robinjmm@users.noreply.github.com>
2023-03-23 12:49:20 +0800

    Setup project to use Vite as the bundler

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import {defineConfig} from "vite";
+import {createHtmlPlugin} from "vite-plugin-html";
+import postcssPresetEnv from "postcss-preset-env";
+import purgeCSSPlugin from "@fullhuman/postcss-purgecss";
+
+export default defineConfig({
+	plugins: [
+		createHtmlPlugin({
+			minify: true,
+		}),
+	],
+	css: {
+		postcss: {
+			plugins: [
+				postcssPresetEnv,
+				purgeCSSPlugin({
+					content: ["./index.html"],
+					variables: true,
+					keyframes: true,
+				}),
+			],
+		},
+	},
+});
